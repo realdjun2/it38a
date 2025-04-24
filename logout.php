7750cf4827c0f14e7d397ab1adc91423e55bda65
Realdjun Gallentes Odon <143726941+realdjun2@users.noreply.github.com>
2025-04-24 15:57:59 +0800

    Updated logout odon.php

--- logout.php
+++ logout.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
+
+// Unset all session variables
+session_unset();
+
+// Destroy the session
 session_destroy();
+
+// Ensure that session cookies are deleted by setting them with a past expiration date
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 3600, '/');
+}
+
+// Redirect to login page
 header("Location: login.php");
 exit();
 ?>
+
